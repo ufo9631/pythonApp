7c39ecc2e24596ff29c9db982cd6b0dc861b9ef0
编程猴子无大脑 <695901733@qq.com>
2018-10-23 23:25:47 +0800

    数据库操作

--- txtToDb.py
+++ txtToDb.py
@@ -1,5 +1,5 @@
 
-
+import pymysql
 nameList=[]
 scoreList=[]
 countryList=[]
@@ -8,7 +8,7 @@ def readTxt():
     #文件读取 read(),readline(),readlines()
     line=txtFile.readline()
     while line: #表示line有值
-        str=line.split() #不指定分隔符  默认使用空格
+        str=line.split('\t') #不指定分隔符  默认使用空格
         name=str[0].split(':')[1]
         score=str[1].split(':')[1]
         country=str[2].split(':')[1]
@@ -19,3 +19,34 @@ def readTxt():
     txtFile.close()
 
 readTxt()
+
+
+'''
+数据入库
+mysqldb python2 不支持python3
+pymysql 支持python3
+mysql.connector --mysql官方发布
+'''
+def operDB():
+    conn=pymysql.connect(user='root',password='Keytop:wabjtam!',host='localhost',port=5831,db='moivedb')
+    myCursor=conn.cursor()
+    sql="insert into movie(movieName,score,country)values('%s','%s','%s')" #占位符的方式
+    for i in range(250):
+        values=(nameList[i],scoreList[i],countryList[i])
+        myCursor.execute(sql % values)
+    conn.commit()
+    conn.close()
+#readTxt()
+#operDB()
+def getDb():
+    conn=pymysql.connect(user='root',password='Keytop:wabjtam!',host='localhost',port=5831,db='moivedb')
+    myCursor=conn.cursor()
+    sqlSelect="select * from movie where score>9.2"
+    myCursor.execute(sqlSelect)
+    movies=myCursor.fetchall()  #fetchall 得到查询的结果集
+    for movie in movies:
+        print("name:",movies[0])
+    conn.close()
+getDb()
+
+   
